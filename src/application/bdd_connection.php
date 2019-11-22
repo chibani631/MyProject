86a5ac28983b69fca000bdfa1854de4aae5f37ee
yassine <yassine.chibani@abshore.com>
2019-11-22 19:19:44 +0100

    updateBase

--- src/application/bdd_connection.php
+++ src/application/bdd_connection.php
@@ -5,7 +5,7 @@
 	(
 		'mysql:host=localhost;dbname=blog;charset=UTF8',
 		'root',
-		'troiswa',
+		'',
 	    [
 	    	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 	        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
